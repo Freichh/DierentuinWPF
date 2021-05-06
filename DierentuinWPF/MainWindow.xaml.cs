5caaa06a2593fed8d243594a04a15760fcf47677
Freek Heining <freek.heining@gmail.com>
2021-05-06 12:50:12 +0200

    Add project files.

---
+++ DierentuinWPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DierentuinWPF.Models;
+
+namespace DierentuinWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            #region Pseudocode
+            /* Deel 1
+            1. Maak base class Animal, inh Monkey, Lion, Elephant
+            2. De class heeft ook een methode Eat die de energie van een dier doet toenemen met 25
+            3. Maak de GUI
+            4. Is, as, OfType<T> experimenten/uitzoeken
+            * Deel 2
+            5. Methods om dieren te voeren
+            6. Dieren kunnen toevoegen aan main veld met knoppen
+            * Deel 3
+            7. Timer 
+            8. Energiebalk
+            9. Sterfte/verwijderen uit verzameling
+            */
+            #endregion
+                    
+
+        }
+
+        private void AddMonkeyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Monkey test = new Monkey();
+        }
+    }
+}
---
+++ DierentuinWPF/Models/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DierentuinWPF.Models
+{
+    class Animal
+    {
+
+        public string Name { get; set; }
+        public int Energy { get; set; } = 100;
+
+        
+        public static int Eat(int energy)
+        {
+            energy += 25;
+            return energy;
+        }
+
+    }
+
+
+}
